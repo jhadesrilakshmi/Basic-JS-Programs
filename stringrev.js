72a168f4e46b16a9b5c865031e622b75c599f80d
Srilakshmi <jhadesrilakshmi.com>
2026-05-04 14:54:47 +0530

    Text

--- stringrev.js
+++ stringrev.js
@@ -1,3 +1,6 @@
-let a = "Sonu";
-let reversed = a.split('').reverse().join('');
-console.log(reversed);
+let num=1234;
+let rev=0;
+for(let i=num;i>0;i=Math.floor(i/10)){
+    rev=rev*10+i%10;
+}
+console.log(rev);
